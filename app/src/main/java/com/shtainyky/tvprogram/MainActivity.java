56c76d22e04005be3973bbe0fe633e177bfc5800
Irina Shtain <shtain_irina@ukr.net>
2017-01-27 15:32:46 +0200

    Have created NavigationDrawer

--- app/src/main/java/com/shtainyky/tvprogram/MainActivity.java
+++ app/src/main/java/com/shtainyky/tvprogram/MainActivity.java
@@ -1,13 +1,82 @@
 package com.shtainyky.tvprogram;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private Toolbar mToolbar;
+    DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupToolbarMenu();
+        setupNavigationDrawerMenu();
+
     }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        closeDrawer();
+        switch (item.getItemId()) {
+            case R.id.item_tv_program:
+                Toast.makeText(this, "item_tv_program", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item_list_categories:
+                Toast.makeText(this, "item_list_categories", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item_list_channels:
+                Toast.makeText(this, "item_list_channels", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item_list_preferred_channels:
+                Toast.makeText(this, "item_list_preferred_channels", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return false;
+    }
+
+    private void closeDrawer() {
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+    private void setupNavigationDrawerMenu() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this,
+                mDrawerLayout,
+                mToolbar,
+                R.string.drawer_open,
+                R.string.drawer_close);
+        mDrawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+    }
+
+    private void setupToolbarMenu() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.app_name);
+    }
+
+
 }
