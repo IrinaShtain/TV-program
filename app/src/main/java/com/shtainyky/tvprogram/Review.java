92b041ed2a40e037c8854038d476723fee489143
Oleg Zolotar <zolotar1123@gmail.com>
2017-02-20 21:52:01 +0200

    common tips

---
+++ app/src/main/java/com/shtainyky/tvprogram/Review.java
@@ -0,0 +1,13 @@
+package com.shtainyky.tvprogram;
+
+/**
+ * Created by oliver on 20.02.17.
+ */
+
+public class Review {
+    // TODO: 20.02.17 reorganize package structure check rules from https://www.linkedin.com/pulse/android-package-structure-performance-tam-nguyen
+    // TODO: 20.02.17 check code style
+//    oracle :: http://www.oracle.com/technetwork/java/codeconventions-150003.pdf
+//    android ::https://github.com/ribot/android-guidelines/blob/master/project_and_code_guidelines.md
+    // TODO: 20.02.17 use meaningful names
+}
